eeb8b1df5521172027063f4db355c2d63f82e196
andreluis034 <andreluis034@users.noreply.github.com>
2017-12-19 19:20:08 +0000

    Feedback when server is ready

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const config = require('./config.json')
 const http = require('http')
 const URL = require('url')
 const routes = require('./routes')
 const server = http.createServer(routes)
-
-server.listen(8008)
+server.on('listening', () => {
+    console.log(`Listening on http://127.0.0.1:${config.port}`)
+})
+server.listen(config.port)
